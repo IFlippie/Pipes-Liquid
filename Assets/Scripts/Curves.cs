1f67bab80e5c98e045fde65ffc2a3799519d1e71
P.M.J Duijster <flipiefantasy@gmail.com>
2022-10-03 11:45:37 +0200

    first version of bezier curves

--- Assets/Scripts/Curves.cs
+++ Assets/Scripts/Curves.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class Curves : MonoBehaviour
 {
+    public Vector3[] points;
+
+    public void Reset()
+    {
+        points = new Vector3[] {
+            new Vector3(1f, 0f, 0f),
+            new Vector3(2f, 0f, 0f),
+            new Vector3(3f, 0f, 0f)
+        };
+    }
+    public static class Bezier
+    {
+        public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+        {
+            return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+        }
+    }
+    public Vector3 GetPoint(float t)
+    {
+        return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], t));
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +39,56 @@ public class Curves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-}
+
+    [CustomEditor(typeof(Curves))]
+    public class BezierEditor : Editor
+    {
+
+        private Curves curve;
+        private Transform handleTransform;
+        private Quaternion handleRotation;
+        private const int lineSteps = 10;
+
+        public void OnSceneGUI()
+        {
+            curve = target as Curves;
+
+            handleTransform = curve.transform;
+            handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+
+            Vector3 p0 = ShowPoint(0);
+            Vector3 p1 = ShowPoint(1);
+            Vector3 p2 = ShowPoint(2);
+
+            Handles.color = Color.white;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p1, p2);
+
+            Handles.color = Color.white;
+            Vector3 lineStart = curve.GetPoint(0f);
+            for (int i = 1; i <= lineSteps; i++)
+            {
+                Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
+                Handles.DrawLine(lineStart, lineEnd);
+                lineStart = lineEnd;
+
+            }
+        }
+            private Vector3 ShowPoint(int index)
+            {
+                Vector3 point = handleTransform.TransformPoint(curve.points[index]);
+                EditorGUI.BeginChangeCheck();
+                point = Handles.DoPositionHandle(point, handleRotation);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(curve, "Move Point");
+                    EditorUtility.SetDirty(curve);
+                    curve.points[index] = handleTransform.InverseTransformPoint(point);
+                }
+                return point;
+            }
+
+        }
+    }
