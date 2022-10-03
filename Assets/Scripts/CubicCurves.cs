4bfa6bdcc1b493f68154a1e02675feff9b96766d
P.M.J Duijster <flipiefantasy@gmail.com>
2022-10-03 15:29:43 +0200

    added terrain and added new bezier curve script

---
+++ Assets/Scripts/CubicCurves.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubicCurves : MonoBehaviour
+{
+    public Vector3[] points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeCurve() 
+    {
+        int length = points.Length;
+        for (int  i = 0;  i <length;  i++)
+        {
+
+        }
+    }
+}
