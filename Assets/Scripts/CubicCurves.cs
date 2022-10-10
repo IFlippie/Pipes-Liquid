bd828aceae644c9ca4854b33a55982bc6cbe9f01
P.M.J Duijster <flipiefantasy@gmail.com>
2022-10-10 15:58:40 +0200

    added raycasts to the extra points

--- Assets/Scripts/CubicCurves.cs
+++ Assets/Scripts/CubicCurves.cs
@@ -21,7 +21,7 @@ public class CubicCurves : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit)) 
             {
-                Debug.DrawLine(transform.position, hit.point, Color.green, Vector3.Distance(transform.position, hit.point));
+                Debug.DrawLine(transform.position, hit.point, Color.green, 10000f);
 
                 //print(Vector3.Angle(hit.normal, ray.direction));
                 MakeCurve(ray, hit);
@@ -33,14 +33,25 @@ public class CubicCurves : MonoBehaviour
     {
         float dist = Vector3.Distance(transform.position, hit.point);
         float stepDist = dist / stepSize;
-        print(stepDist);
-        for (int  i = 0;  i < stepSize;  i++)
-        {
+        //print(stepDist);
+        for (int  i = 1;  i < stepSize;  i++)
+        {          
             Vector3 pos = transform.position + (hit.point - transform.position).normalized * (stepDist * i);
+
+            Debug.DrawRay(pos, Vector3.up * 3f, Color.red, 10000f);
+            Debug.DrawRay(pos, Vector3.down * 3f, Color.red, 10000f);
+            Debug.DrawRay(pos, Vector3.left * 3f, Color.red, 10000f);
+            Debug.DrawRay(pos, Vector3.right * 3f, Color.red, 10000f);
+
+            if (Physics.Raycast(pos,Vector3.up * 3f ,out RaycastHit upHit)) { }
+            if (Physics.Raycast(pos, Vector3.down * 3f, out RaycastHit downHit)) { }
+            if (Physics.Raycast(pos, Vector3.left * 3f, out RaycastHit leftHit)) { }
+            if (Physics.Raycast(pos, Vector3.right * 3f, out RaycastHit rightHit)) { }
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube.transform.position = pos;
-            print("cube point is: " + cube.transform.position);
-            points.Add(pos);         
+
+            //print("cube point is: " + cube.transform.position);
+            points.Add(pos);
         }
         //print("end point is: " + hit.point);
     }
